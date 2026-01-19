0b07db95c26f3b2e6cd4966681bb69b3a779386a
Paras <pan.paras07@gmail.com>
2026-01-19 08:34:55 +0530

    fix dataloader and unet architecture, improve training loop

--- semdronedataset.py
+++ semdronedataset.py
@@ -3,49 +3,53 @@ from torch.utils.data import Dataset
 import numpy as np
 from PIL import Image
 import os
+import cv2
 
 
 class SemanticDroneDataset(Dataset):
-    def __init__(
-        self,
-        images_dir: str,
-        original_foldername: str,
-        masks_foldername: str,
-        transform=None,
-    ) -> None:
+    def __init__(self, images_dir: str, transform=None) -> None:
         self.transform = transform
         self.images_dir = images_dir
-        self.images_path = os.path.join(images_dir, original_foldername)
-        self.masks_path = os.path.join(images_dir, masks_foldername)
-        self.images = os.listdir(os.path.join(images_dir, original_foldername))
-        self.masks = os.listdir(os.path.join(images_dir, masks_foldername))
+        self.images_path = os.path.join(images_dir, "original")
+        self.masks_path = os.path.join(images_dir, "mask")
+        self.images = sorted(os.listdir(os.path.join(images_dir, "original")))
+        self.masks = sorted(os.listdir(os.path.join(images_dir, "mask")))
 
     def __len__(self) -> int:
         return len(self.images)
 
-    def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
+    def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor, str]:
         img = torch.tensor(
-            np.transpose(
-                np.array(
-                    Image.open(
-                        os.path.join(self.images_path, self.images[idx])
-                    ).convert("RGB")
-                ),
-                axes=(2, 0, 1),
-            ),
-            dtype=torch.float32,
+            np.transpose(np.array(
+                Image.open(os.path.join(self.images_path, self.images[idx])).convert(
+                    "RGB"
+                )
+            ), axes=(2, 0, 1)),
+            dtype=torch.float32
         )
         mask = torch.tensor(
             np.array(
                 Image.open(os.path.join(self.masks_path, self.masks[idx])),
             ),
-            dtype=torch.long,
+            dtype=torch.long
         )
 
         if self.transform:
             img = self.transform(img)
+            mask.unsqueeze_(0)
             mask = self.transform(mask)
+            mask.squeeze_(0)
+        
+        return img, mask, self.images[idx]
 
-        # img = img.permute(2, 0, 1)
-        # mask = mask.permute(2, 0, 1)
-        return img, mask
+if __name__ == "__main__":
+    from torch.utils.data import DataLoader
+    data = SemanticDroneDataset("archive/classes_dataset/classes_dataset/")
+    loader = DataLoader(data)
+    for i, j, name in loader:
+        print(i.shape, j.shape)
+        # Check if the pair of input image and the segmented image are the same.
+        cv2.imshow("a", np.transpose(i.numpy()[0], axes=(1, 2, 0)).astype(np.uint8))
+        cv2.imshow("b", j.numpy()[0].astype(np.uint8)*40)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
--- training.py
+++ training.py
@@ -3,10 +3,12 @@ from torch.utils.data import DataLoader
 from semdronedataset import SemanticDroneDataset
 from unet import UNet
 import matplotlib.pyplot as plt
+from torchvision import transforms
+import torchvision.transforms.functional as TF
 
-
-EPOCHS = 100
-BATCH_SIZE = 1
+# HYPERPARAMETERS
+EPOCHS = 125
+BATCH_SIZE = 11
 LEARNING_RATE = 5e-5
 if torch.cuda.is_available():
     DEVICE = "cuda"
@@ -17,19 +19,9 @@ else:
 SHUFFLE = True
 PIN_MEMORY = True
 SAVE_DIR = "./model_save/binary.pth"
-NUM_WORKERS = 8
-PREFETCH_FACTOR = 2
+NUM_WORKERS = 16
+PREFETCH_FACTOR = 16
 
-
-dataset = SemanticDroneDataset("./archive/classes_dataset/classes_dataset/", "original", "mask")
-loader = DataLoader(
-    dataset,
-    batch_size=BATCH_SIZE,
-    shuffle=SHUFFLE,
-    pin_memory=PIN_MEMORY,
-    num_workers=NUM_WORKERS,
-    prefetch_factor=PREFETCH_FACTOR,
-)
 
 plt.ion()
 fig, ax = plt.subplots()
@@ -45,53 +37,66 @@ loss_text = ax.text(
     verticalalignment="top"
 )
 
+
+dataset = SemanticDroneDataset("./archive/classes_dataset/classes_dataset/", transform=transforms.Resize((368, 480)))
+loader = DataLoader(
+    dataset,
+    batch_size=BATCH_SIZE,
+    shuffle=SHUFFLE,
+    pin_memory=PIN_MEMORY,
+    num_workers=NUM_WORKERS,
+    prefetch_factor=PREFETCH_FACTOR,
+)
+
+
+weights = torch.zeros(5).to(DEVICE)
+for batch_feats, batch_outs, name in loader:
+    for i in range(0, 5):
+        weights[i] += torch.sum(batch_outs == i)
+
+weights = 1 / weights
+weights = weights / weights.sum().item()
+
 model = UNet()
 print(f"Using device {DEVICE}")
 model = model.to(DEVICE)
 optim = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
-loss_fn = torch.nn.CrossEntropyLoss()
+loss_fn = torch.nn.CrossEntropyLoss(weight=weights)
 
-for i in range(EPOCHS):
-    loss_avg = 0
-    for batch_feats, batch_outs in loader:
-        batch_feats, batch_outs = batch_feats.to(DEVICE), batch_outs.to(DEVICE)
-        # print(batch_outs)
-        # print(batch_outs.min(), batch_outs.max())
-        
-        batch_feats /= 255
+try:
+    for i in range(EPOCHS):
+        loss_avg = 0
+        for batch_feats, batch_outs, name in loader:
+            batch_feats, batch_outs = batch_feats.to(DEVICE)/255, batch_outs.to(DEVICE)
 
-        A = model(batch_feats)
-        # A.squeeze_(1)
-        # print(A.shape, batch_outs.shape)
-        loss = loss_fn(A, batch_outs)
+            A = model(batch_feats)
+            loss = loss_fn(A, TF.resize(batch_outs, size=A.shape[2:]))
 
-        # print('LOSSFN')
+            optim.zero_grad()
+            loss.backward()
+            
+            optim.step()
+            loss_avg += loss.item()
+            losses.append(loss.item())
+            
+            line.set_data(range(1, len(losses) + 1), losses)
+            loss_text.set_text(f"Current Loss: {losses[-1]}")
 
-        optim.zero_grad()
-        loss.backward()
+            ax.relim()
+            ax.autoscale_view()
+            fig.canvas.draw()
+            fig.canvas.flush_events()
 
-        # print("backprop")
-        optim.step()
-        loss_avg += loss.item()
-        losses.append(loss.item())
-        # print(loss.item())
-        # ---- update plot ----
-        line.set_data(range(1, len(losses) + 1), losses)
-        loss_text.set_text(f"Current Loss: {losses[-1]}")
+        loss_avg /= len(loader)
 
-        ax.relim()
-        ax.autoscale_view()
-        fig.canvas.draw()
-        fig.canvas.flush_events()
+        print(f"EPOCH {i+1}, COST = {loss_avg}")
 
-    loss_avg /= len(loader)
+except KeyboardInterrupt:
+    print("Haulting training.")
 
-
-
-    print(f"EPOCH {i+1}, COST = {loss_avg}")
+finally:
+    print(f"MODEL TRAINED!\nSaved at {SAVE_DIR}")
+    torch.save(model.state_dict(), SAVE_DIR)
 
 plt.ioff()
 plt.show()
-
-print(f"MODEL TRAINED!\nSaved at {SAVE_DIR}")
-torch.save(model.state_dict(), SAVE_DIR)
